a49cce26b18ea4b8376470aa028e88ab6617e567
UnluckyLuki <themrluki43@gmail.com>
2023-11-16 13:43:26 +0100

    Using React Hooks

--- src/Pages/LoginPage/LoginWindow.js
+++ src/Pages/LoginPage/LoginWindow.js
@@ -2,6 +2,11 @@ import ReportBug from "../../ReportBug";
 import Accesibilities from "../../Accesibilities";
 import LoginModel from "../../Components/Models/LoginModel";
 function LoginWindow(){
+    console.log(LoginModel.error)
+    const {
+        error,
+        handleLogin
+    } = LoginModel()
     return(
         <div className="login-window">
             <div className="left-section">
@@ -12,18 +17,18 @@ function LoginWindow(){
                 <div className="cover"/>
                 <span className="shadow"></span>
                 <div className="login-form">
-                <form onSubmit={LoginModel.handleLogin}>
+                <form onSubmit={handleLogin}>
                     <h1>Logowanie</h1>
                     <div className="input">
                         <label>Username</label>
                         <input type="text" name="username" required/>
-                        <div className="username-error"> error</div>
+                        {/*<div className="username-error"> error</div>*/}
                     </div>
                     <div className="input">
                         <label>Password</label>
                         <input type="text" name="password" required/>
-                        <div className="username-error"> error</div>
                     </div>
+                    <div className="login-error">{error.visible ? error.message : ""}</div>
                     <a href="#">Register here.</a>
                     <div className="submit-button">
                         <input type="submit" value="Zaloguj się"/>
