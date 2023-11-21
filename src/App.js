26875e5aa0012c7f7ce9c064ef207b800df25b90
UnluckyLuki <themrluki43@gmail.com>
2023-11-21 19:57:48 +0100

    Protected route with bad css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-
-
-
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+import {Link, Route, Routes} from "react-router-dom";
+import ProtectedRoute from "./Components/ProtectedRoutes/ProtectedRoutes";
+import {useState} from "react";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import RegistrationPage from "./Pages/RegistrationPage/RegistrationPage";
+import AccountPage from "./Pages/AccountPage/AccountPage";
+import CoursePage from "./Pages/CoursePage/CoursePage";
+const App = () => {
+    //TODO: Znaleść sposób na zalezienie usera tutaj
+    const [user, setUser] = useState({id: 1, username: "Pepe", password: null, email: "pepe@pepe.pepe"})
+    return(
+        <Routes>
+            <Route index element={<LoginPage/>}/>
+            // Dostępne
+            <Route path="login" element={<LoginPage/>}/>
+            <Route path="register" element={<RegistrationPage/>}/>
+            <Route path="*" element={<p>Nic tu nie ma gałganie</p>}/>
+            // Po zalogowaniu
+            <Route element={<ProtectedRoute user ={user}/>}>
+                <Route path="account" element={<AccountPage/>}/>
+                <Route path="course" element={<CoursePage/>}/>
+            </Route>
+            // TODO: Reszta
+        </Routes>
+    )
 }
+
+/// Przykład nawigacji
+const Navigation = () => {
+    return (
+        <nav>
+            <Link to="/login">login</Link>
+            <Link to="/register">register</Link>
+        </nav>
+    );
+};
 
 export default App;
